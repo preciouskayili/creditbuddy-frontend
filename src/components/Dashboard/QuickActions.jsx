f3603c1112801fcfab7b97d46008c5a665a83435
preciouskayili <preciouskayili@gmail.com>
2023-02-14 03:03:19 +0100

    ui: quick actions

--- src/components/Dashboard/QuickActions.jsx
+++ src/components/Dashboard/QuickActions.jsx
@@ -8,24 +8,24 @@ const QuickActions = () => {
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-12 gap-4">
-        <div className="bg-white rounded-lg shadow p-4 col-span-6">
-          <UilWallet className="text-green-500 text-3xl mb-4" />
-          <h3 className="text-lg font-semibold mb-2">Deposit</h3>
-          <p className="text-gray-600">
-            Quickly deposit funds into your account
-          </p>
+        <div className="bg-white/20 border border-white/20 rounded-lg shadow p-4 col-span-6 flex items-center">
+          <UilWallet className="text-green-500 text-3xl rounded-full bg-black/40 p-3 h-12 w-12 mr-2" />
+          <div>
+            <h3 className="text-lg font-semibold">Airtime</h3>
+            <small>Pay bills na</small>
+          </div>
         </div>
-        <div className="bg-white rounded-lg shadow p-4 col-span-6">
-          <UilExchangeAlt className="text-blue-500 text-3xl mb-4" />
-          <h3 className="text-lg font-semibold mb-2">Transfer</h3>
-          <p className="text-gray-600">
-            Easily transfer money between accounts
-          </p>
+        <div className="bg-white/20 border border-white/20 rounded-lg shadow p-4 col-span-6 flex items-center">
+          <UilWallet className="text-green-500 text-3xl rounded-full bg-black/40 p-3 h-12 w-12 mr-2" />
+          <h3 className="text-lg font-semibold">Data</h3>
         </div>
-        <div className="bg-white rounded-lg shadow p-4 col-span-6">
-          <UilInvoice className="text-purple-500 text-3xl mb-4" />
-          <h3 className="text-lg font-semibold mb-2">Pay</h3>
-          <p className="text-gray-600">Make payments to vendors or billers</p>
+        <div className="bg-white/20 border border-white/20 rounded-lg shadow p-4 col-span-6 flex items-center">
+          <UilWallet className="text-green-500 text-3xl rounded-full bg-black/40 p-3 h-12 w-12 mr-2" />
+          <h3 className="text-lg font-semibold">Electricity</h3>
+        </div>
+        <div className="bg-white/20 border border-white/20 rounded-lg shadow p-4 col-span-6 flex items-center">
+          <UilWallet className="text-green-500 text-3xl rounded-full bg-black/40 p-3 h-12 w-12 mr-2" />
+          <h3 className="text-lg font-semibold">Cable TV</h3>
         </div>
       </div>
     </div>
