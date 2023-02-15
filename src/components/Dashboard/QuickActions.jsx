479627ba56ce1484ad83eb22dff2a20f6e956768
preciouskayili <preciouskayili@gmail.com>
2023-02-15 16:43:03 +0100

    update: quick actions

--- src/components/Dashboard/QuickActions.jsx
+++ src/components/Dashboard/QuickActions.jsx
@@ -7,34 +7,34 @@ import {
 const QuickActions = () => {
   return (
     <div className="grid gap-3 grid-cols-12">
-      <button className="col-span-6 bg-yellow-300/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
+      <button className="col-span-6 bg-blue-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
         <div className="text-sm h-12 w-12 p-3 flex items-center justify-center bg-black/50 rounded-full">
           <UilArrowFromTop />
         </div>
-        <h3 className="font-bold text-white mt-3 text-left text-md">Airtime</h3>
+        <h3 className="font-bold text-white mt-3 text-left text-sm tracking-wide">Airtime</h3>
       </button>
 
-      <button className="col-span-6 bg-orange-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
+      <button className="col-span-6 bg-blue-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
         <div className="text-sm h-12 w-12 p-3 flex items-center justify-center bg-black/50 rounded-full">
           <UilArrowFromTop />
         </div>
-        <h3 className="font-bold text-white mt-3 text-left text-md">Data</h3>
+        <h3 className="font-bold text-white mt-3 text-left text-sm tracking-wide">Data</h3>
       </button>
 
-      <button className="col-span-6 bg-cyan-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
+      <button className="col-span-6 bg-blue-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
         <div className="text-sm h-12 w-12 p-3 flex items-center justify-center bg-black/50 rounded-full">
           <UilArrowFromTop />
         </div>
-        <h3 className="font-bold text-white mt-3 text-left text-md">
+        <h3 className="font-bold text-white mt-3 text-left text-sm tracking-wide">
           Electricity
         </h3>
       </button>
 
-      <button className="col-span-6 bg-teal-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
+      <button className="col-span-6 bg-blue-500/10 transition-all rounded-xl px-4 py-3 text-xs text-white/80">
         <div className="text-sm h-12 w-12 p-3 flex items-center justify-center bg-black/50 rounded-full">
           <UilArrowFromTop />
         </div>
-        <h3 className="font-bold text-white mt-3 text-left text-md">
+        <h3 className="font-bold text-white mt-3 text-left text-sm tracking-wide">
           Cable TV
         </h3>
       </button>
